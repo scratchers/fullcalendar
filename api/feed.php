51a2b2e76b9ecd356e4cbd63b2c12893ecdcbd40
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-13 04:01:35 +0000

    use database records

--- api/feed.php
+++ api/feed.php
@@ -2,33 +2,8 @@
 
 header('Content-Type: application/json');
 
-echo '[
-    {
-        "title"  : "event1",
-        "start"  : "2017-01-01"
-    },
-    {
-        "title"  : "event2",
-        "start"  : "2017-01-05",
-        "end"    : "2017-01-07",
-        "color"  : "yellow",
-        "textColor": "black"
-    },
-    {
-        "title"  : "event3",
-        "start"  : "2017-01-09T12:30:00",
-        "allDay" : false
-    }
-]';
+$pdo = require __DIR__.'/../pdo.php';
 
-// $events = array();
-// $i = 10;
-// while($i--) {
-//     $e = array();
-//     $e['id'] = $i;
-//     $e['title'] = "Event number $i";
-//     $e['start'] = "2017-01-".$i*2;
-//     $e['allDay'] = true;
-//     array_push($events, $e);
-// }
-// echo json_encode($events);
+$sql = 'SELECT * FROM `events`';
+
+echo json_encode($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC));
